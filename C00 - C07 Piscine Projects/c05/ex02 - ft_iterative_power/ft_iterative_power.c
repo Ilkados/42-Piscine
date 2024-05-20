cfdec81a8241bc209f5527b2eaf66b1f9b4d810d
ilyas <168590814+Ilkados@users.noreply.github.com>
2024-05-21 00:21:10 +0200

    Update ft_iterative_power.c
    i have just the change nb to base, just for more understanding and also for give the variable meaing

--- C00 - C07 Piscine Projects/c05/ex02 - ft_iterative_power/ft_iterative_power.c
+++ C00 - C07 Piscine Projects/c05/ex02 - ft_iterative_power/ft_iterative_power.c
@@ -1,18 +1,18 @@
-int	ft_iterative_power(int nb, int power)
+int	ft_iterative_power(int base, int power)
 {
 	int	i;
 
-	i = nb;
+	i = base;
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
 	while (power > 1)
 	{
-		nb *= i;
+		base *= i;
 		power--;
 	}
-	return (nb);
+	return (base);
 }
 /*
 #include <stdio.h>
